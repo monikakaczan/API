9c3f58c8664135f3e3493f040cf2cb384c37feeb
Monika Kaczanowska <kaczanowskamonika@gmail.com>
2020-05-24 15:02:50 +0100

    test first get request

--- test.js
+++ test.js
@@ -5,3 +5,15 @@ let should = require("should");
 var expect = chai.expect;
 import app from './index'
 chai.use(chaiHttp);
+
+describe ('App API', () => {
+    it ('renders the homepage', (done) => {
+        chai.request(app)
+        .get('/')
+        .end((err, res) => {
+            if (err) done(err); 
+            expect(res).to.have.status(200)
+            done()
+        })
+    })
+})
