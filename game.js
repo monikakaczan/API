148f45fef07f3df4edb31cb2718e146859bc7a27
Monika Kaczanowska <kaczanowskamonika@gmail.com>
2020-05-24 14:22:19 +0100

    set up game class and api submit entry

---
+++ game.js
@@ -0,0 +1,24 @@
+class Game {
+    static submitEntry = (entry) => {
+        const record = {
+            name: entry.name,
+            word: entry.word,
+            points: 0
+        }
+
+    }
+
+    static isPalindrome = (record) => { 
+
+    }
+
+    static getScore = (record) => {
+
+    }
+
+    static getScores = () => {
+        
+    }
+}
+
+export default Game;
--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 var express = require('express');
-
+import Game from './game'
 var app = express();
 
 app.use(express.static(__dirname));
+app.use(express.json())
+
 
 app.get('/', function(req, res) {
 	res.render('index.html');
 });
+
+app.post('/api/submitEntry', async (req, res) => {
+	try {
+		const entry = {
+			name: req.body.name,
+			word: req. body. word
+		}
+		const entries = await Game.submitEntry(entry)
+		res.status(200).json(entries)
+	} catch (err) {
+		console.log(err)
+	}
+})
 
 var port = 3000;
 app.listen(port, function() {
