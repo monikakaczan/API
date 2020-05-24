ec80adb0048b10f3cb5be2dce3235ce398946fe7
Monika Kaczanowska <kaczanowskamonika@gmail.com>
2020-05-24 23:14:39 +0100

    add tests

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ app.post('/api/submitEntry', async (req, res) => {
 			word: req. body. word
 		}
 		const entries = await Game.submitEntry(entry)
+		console.log('entries', entries)
 		res.status(200).json(entries)
 	} catch (err) {
 		console.log(err)
---
+++ test/test.js
@@ -0,0 +1,122 @@
+const chai = require('chai');
+let chaiHttp = require('chai-http');
+let request = require("request");
+let should = require("should");
+import app from '../index'
+
+const { expect } = chai;
+chai.use(chaiHttp);
+
+describe ('App API', () => {
+  
+    const testData = [
+        {
+            name: 'user1',
+            word: 'Hello'
+         },
+         {
+            name: 'user2',
+            word: 'a man a plan a canal panama'
+         },
+         {
+            name: 'user3',
+            word: 'Was it a cat I saw'
+         },
+         {
+            name: 'user4',
+            word: 'Eva can I see bees in a cave'
+         },
+         {
+            name: 'user5',
+            word: 'No lemon no melon'
+         },
+         {
+            name: 'user6',
+            word: '111111111111111111111111111111111111111111111111'
+         },
+         {
+            name: 'user7',
+            word: 'Anna'
+         },
+         {
+            name: 'user8',
+            word: 'Top spot'
+         }
+    ]
+
+    it ('renders the homepage', (done) => {
+        chai.request(app)
+        .get('/')
+        .end((err, res) => {
+            if (err) done(err); 
+            expect(res).to.have.status(200)
+            done()
+        })
+    })
+    describe('/api/submitEntry', () => {
+        it('submits an entry and returns a name and a score', (done) => {
+            chai
+            .request(app)
+            .post('/api/submitEntry')
+            .send(
+                testData[1]
+            )
+            .end((err, res) => {
+                if (err) done(err);
+                expect(res).to.have.status(200);
+                expect(res).to.be.an('object')
+                expect(res.body).to.eql([ { name: 'user2', points: 21 } ])
+                done()
+            })
+        })
+    })
+   
+    it('does not submit an entry if the input is not a palindrome',  (done) => {
+        chai
+        .request(app)
+        .post('/api/submitEntry')
+        .send(
+            testData[0]
+        )
+        .end((err, res) => {
+            if (err) done(err);
+            expect(res).to.have.status(200);
+            expect(res).to.be.an('object')
+            expect(res.body).to.eql([])
+            done()
+        })
+    })
+    
+    describe('/api/getScores', () => {
+        
+        it('returns max 5 scores', (done) => {
+            chai
+            .request(app)
+            .post('/api/submitEntry')
+            .send(testData[0])
+            .end(() => {
+                chai.request(app).post('/api/submitEntry').send(testData[1]).end(() => {
+                    chai.request(app).post('/api/submitEntry').send(testData[2]).end(() => {
+                        chai.request(app).post('/api/submitEntry').send(testData[3]).end(() => {
+                            chai.request(app).post('/api/submitEntry').send(testData[4]).end(() => {
+                                chai.request(app).post('/api/submitEntry').send(testData[5]).end(() => {
+                                    chai.request(app).post('/api/submitEntry').send(testData[6]).end(() => {
+                                        chai.request(app).post('/api/submitEntry').send(testData[7]).end(() => {
+                                            chai.request(app).get('/api/getScores').end((err,res) => {
+                                                if (err) done(err);
+                                                    expect(res).to.have.status(200);
+                                                    expect(res.body).to.have.lengthOf(5)
+                                                    done()
+                                            })
+                                                    
+                                            })
+                                        })
+                                    })
+                                })
+                            })
+                        })
+                    })
+                })
+            })
+    })
+})
