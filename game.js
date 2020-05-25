6f39336e4126581986f4ca88fb4db3ac2c34d3a3
Monika Kaczanowska <kaczanowskamonika@gmail.com>
2020-05-25 14:32:26 +0100

    fix further linting issues

--- game.js
+++ game.js
@@ -6,50 +6,50 @@ class Game {
             name: entry.name,
             word: entry.word,
             points: 0
-        }
-        const input = this.isPalindrome(record)
+        };
+        const input = this.isPalindrome(record);
 
         if (input) {
             const entry = { 
                 name: record.name,
                 points: this.getScore(record)
             }
-            scores.push(entry)
+            scores.push(entry);
         }
-        return this.getScores(scores)
-    }
+        return this.getScores(scores);
+    };
     //check if the word is a palindrome
     static isPalindrome = (record) => { 
         // remove white spaces and special characters 
-        const input = record.word.toLowerCase().replace(/[-\s/\\^$*+?",.()|[\]{}]/g, '')
+        const input = record.word.toLowerCase().replace(/[-\s/\\^$*+?",.()|[\]{}]/g, '');
         // return a boolean
         return input == input.split("").reverse().join("") ? true : false 
-    }
+    };
 
     static getScore = (record) => {
         
-        let score = 0 
+        let score = 0;
         // make sure spaces aren't counted as points 
-        const input = record.word.replace(/\s/g,"")
-        const l = input.length
+        const input = record.word.replace(/\s/g,"");
+        const l = input.length;
         for (let i=0; i < l; i++) {
             score +=1
         }
-        return score 
-    }
+        return score;
+    };
 
     static getScores = () => {
 
         while ( scores.length <= 5 ) {
             const sorted = scores.sort((a,b) => b.points - a.points)
-            return sorted 
+            return sorted;
         } 
         if ( scores.length > 5) {
             const sorted = scores.sort((a,b) => b.points - a.points).slice(0,5)
-            return sorted 
+            return sorted ;
         }
-        return sorted
-    }
-}
+        return sorted;
+    };
+};
 
 export default Game;
